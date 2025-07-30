b10f463eb2bd8c984f5ade0833bc9bb436dbfbe4
QY C <2647097417@qq.com>
2025-07-30 23:08:55 +0800

    求和

---
+++ Chapter2/Chapter219.c
@@ -0,0 +1,17 @@
+﻿# include <stdio.h>
+
+int main()
+{
+	int sign = 1;	// 符号
+	float done = 2.0, sum = 1.0, term;	// 分母 求和 分数
+
+	while (done <= 100)	// while循环
+	{
+		sign =-1* sign;		// 符合
+		term=sign  / done ;	// 分数
+		sum = sum + term;	// 求和
+		done = done+ 1;		// 分母累加
+	}
+	printf("%f \n", sum);	// 输出求和值
+	return 0;
+}
