34bf5a9b1bccab86b968c52f8de77ccb86e25be6
QY C <2647097417@qq.com>
2025-08-04 21:47:17 +0800

    第五章代码

--- Chapter4/Example4_5.c
+++ Chapter4/Example4_5.c
@@ -1,14 +1,20 @@
-#include<stdio.h>
-int main()
-{
-	float x, y;
-	printf("请输入X的值:");
-	scanf_s("%f", &x);
-	if (x < 0)
-		y = -1;
-	else
-		if (x == 0)y = 0;
-		else y = 1;
-	printf("x=%f y=%f\n", x, y);
-		return 0;
-}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	float x, y;
+//	printf("请输入X的值:");
+//	scanf_s("%f", &x);
+//	if (x < 0)
+//		y = -1;
+//	else
+//	{
+//		if (x == 0)
+//			y = 0;
+//		else
+//			y = 1;
+//	}
+//		
+//	printf("x=%f y=%f\n", x, y);
+//	return 0;
+//}
---
+++ Chapter4/example4_7.c
@@ -0,0 +1,40 @@
+////#include<stdio.h>
+////int main()
+////{
+////	char grade;
+////	scanf_s("%c", &grade);
+////	printf("Your score£º");
+////	switch (grade)
+////	{
+////		case 'A':
+////			printf("85~100\n"); 
+////			break;
+////		case 'B':
+////			printf("70~84\n"); 
+////			break;
+////		case 'C':
+////			printf("60~69\n");
+////			break;
+////		case 'D':
+////			printf("<60\n");
+////			break;
+////		default:
+////			printf("enter data error!\n");
+////	}
+////	return 0;
+////}
+//#include <stdio.h>
+//#include <time.h>
+//#include <windows.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	while(i<10)
+//	{	
+//		printf("Hello World  -%d-!\n",i);
+//		i++;
+//		Sleep(100);
+//	}
+//	return 0;
+//}
